248d14c241ea772cce993fe082fe5400647e883a
Evgenii.Kuzovlev <kuzovluft@gmail.com>
2025-03-30 17:27:25 +0700

    common/common: cleanup

--- common/common.go
+++ common/common.go
@@ -23,11 +23,3 @@ func ReverseBytes(data []byte) {
 		data[i], data[j] = data[j], data[i]
 	}
 }
-
-// func wordsToBytes(words []big.Word) []byte {
-// 	bytes := make([]byte, len(words)*8) // Each big.Word is 8 bytes on 64-bit systems
-// 	for i, word := range words {
-// 		binary.BigEndian.PutUint64(bytes[i*8:(i+1)*8], uint64(word))
-// 	}
-// 	return bytes
-// }
